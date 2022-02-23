949fbdd3e3c6c3d2d2e05eb1d93b0ea517ee6df9
Marcus Mouyat <marcus@mouyat.com>
2022-02-23 17:32:33 +1100

    Object viewer class
    This somewhat simple script allows the player to pan, zoom and rotate a particular object (in this case the banknotes) in order to easily view each part of it. This controls entirely using the mouse. Drag while holding right mouse to rotate, or the middle mouse to pan. Use the scroll wheel to zoom, and click the reset button on a HUD canvas to revert the position and rotation back to normal.
    
    This code required me to set up an action asset using Unity's new Input System.

---
+++ Assets/Scripts/ObjectViewer.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectViewer : MonoBehaviour
+{
+    public Transform viewedObject;
+
+
+
+    [Header("Positioning")]
+    public float panSensitivity = 2;
+    public float zoomSensitivity = 1;
+    public Bounds positionBounds = new Bounds(Vector3.forward * 10, Vector3.one * 10);
+
+    [Header("Rotation")]
+    public float rotationSensitivity = 30;
+
+    [Header("Reset")]
+    public UnityEngine.UI.Button resetButton;
+    public float resetTime = 0.5f;
+    public AnimationCurve resetCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    private void Start()
+    {
+        resetButton.onClick.AddListener(OnReset);
+        OnReset();
+    }
+
+    public void OnPanControl(InputValue input)
+    {
+        isPanning = input.isPressed;
+        isRotating = false;
+    }
+    public void OnRotateControl(InputValue input)
+    {
+        isRotating = input.isPressed;
+        isPanning = false;
+    }
+    public void OnMove(InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>();
+
+        if (isPanning)
+        {
+            Vector3 values = panSensitivity * Time.deltaTime * value;
+            viewedObject.localPosition = viewedObject.localPosition + values;
+            ClampPosition();
+        }
+        if (isRotating)
+        {
+            value = new Vector2(value.y, value.x);
+            viewedObject.Rotate(rotationSensitivity * Time.deltaTime * value, Space.Self);
+            Vector3 direction = viewedObject.localRotation * Vector3.forward;
+            viewedObject.localRotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
+
+    public void OnZoom(InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>();
+        Vector3 values = value.y * Time.deltaTime * Vector3.back;
+        viewedObject.localPosition = viewedObject.localPosition + values;
+        ClampPosition();
+    }
+    public void OnReset()
+    {
+        if (resetInProgress != null)
+        {
+            return;
+        }
+        resetInProgress = ResetLook();
+        StartCoroutine(resetInProgress);
+    }
+    IEnumerator ResetLook()
+    {
+        Vector3 oldPosition = viewedObject.localPosition;
+        Quaternion oldRotation = viewedObject.localRotation;
+
+        for (float timer = 0; timer < 1; timer = Mathf.Clamp01(timer + Time.deltaTime / resetTime))
+        {
+            float t = resetCurve.Evaluate(timer);
+            viewedObject.localPosition = Vector3.Lerp(oldPosition, positionBounds.center, t);
+            viewedObject.localRotation = Quaternion.Lerp(oldRotation, Quaternion.identity, t);
+            yield return null;
+        }
+
+        resetInProgress = null;
+    }
+    IEnumerator resetInProgress;
+    bool isPanning;
+    bool isRotating;
+
+    public void ClampPosition()
+    {
+        viewedObject.transform.localPosition = Vector3Clamp(viewedObject.transform.localPosition, positionBounds.min, positionBounds.max);
+    }
+
+    public static Vector3 Vector3Clamp(Vector3 original, Vector3 min, Vector3 max)
+    {
+        original.x = Mathf.Clamp(original.x, min.x, max.x);
+        original.y = Mathf.Clamp(original.y, min.y, max.y);
+        original.z = Mathf.Clamp(original.z, min.z, max.z);
+        return original;
+    }
+
+}
