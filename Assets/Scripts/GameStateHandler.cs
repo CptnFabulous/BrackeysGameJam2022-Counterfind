20902932d14a4ac89c56c6d555ca73260aaad439
Marcus Mouyat <marcus@mouyat.com>
2022-02-24 16:12:39 +1100

    GameStateHandler class for pausing, resuming and end screen
    This class allows the game to easily switch between the pause menu, regular game and end screen, since only one of those states can be happening at a time.

---
+++ Assets/Scripts/GameStateHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateHandler : MonoBehaviour
+{
+    public Canvas headsUpDisplay;
+    public Canvas pauseMenu;
+    public Canvas endScreen;
+    public Button pauseButton;
+    public Button resumeButton;
+
+    private void Awake()
+    {
+        pauseButton.onClick.AddListener(PauseGame);
+        resumeButton.onClick.AddListener(ResumeGame);
+    }
+    private void Start()
+    {
+        ResumeGame();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        SwitchCanvas(pauseMenu);
+    }
+    public void ResumeGame()
+    {
+        SwitchCanvas(headsUpDisplay);
+        Time.timeScale = 1;
+    }
+
+    void SwitchCanvas(Canvas desired)
+    {
+        headsUpDisplay.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(false);
+        endScreen.gameObject.SetActive(false);
+        desired.gameObject.SetActive(true);
+
+    }
+}
