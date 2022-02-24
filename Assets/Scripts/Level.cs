b42ff4a0b6f04605da3c05b0a37e1b86bb78f7c9
Marcus Mouyat <marcus@mouyat.com>
2022-02-24 17:30:53 +1100

    Added default item/counterfeit number stats to the level SciptableObject

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New level", menuName = "ScriptableObjects/Level", order = 0)]
 public class Level : ScriptableObject
 {
-    public int numberOfItems;
-    public int numberOfCounterfeits;
+    public int numberOfItems = 20;
+    public int numberOfCounterfeits = 10;
     public int numberOfErrorsForFailure = 3;
     public Timer.TimeValue timeLimit = new Timer.TimeValue(0, 10, 0);
     // Defects
