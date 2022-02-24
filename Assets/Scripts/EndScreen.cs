739c8e2bc5a679b4fe374e2a9385b16db0063600
Marcus Mouyat <marcus@mouyat.com>
2022-02-24 17:40:01 +1100

    EndScreen class for wins or game overs
    This class manages the menu that appears at the end of a level. It tells the player whether they succeeded or failed, as well as information about their performance such as incorrect guesses and remaining time, and will show different HUD elements based on the success/failure state and the reasons for said state.

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class EndScreen : MonoBehaviour
+{
+    public Text requiredRightForSuccess;
+    public Text successful;
+    public Text thoughtWasReal;
+    public Text thoughtWasFake;
+    public Text remainingTime;
+    public Button retryLevelButton;
+    public Button nextLevelButton;
+
+    public UnityEvent onPerfectWin;
+    public UnityEvent onWin;
+    public UnityEvent onTooFewCheckedAccurately;
+    public UnityEvent onTimeRanOut;
+
+    public void ShowLevelEnd(LevelManager manager)
+    {
+        int finalScore = 0;
+        int amountThoughtReal = 0;
+        int amountThoughtFake = 0;
+        for (int i = 0; i < manager.currentlyChecking; i++)
+        {
+            if (manager.playerVerdicts[i] == manager.allNotes[i].Counterfeit)
+            {
+                finalScore++;
+            }
+            else if (manager.allNotes[i].Counterfeit)
+            {
+                amountThoughtReal++;
+            }
+            else
+            {
+                amountThoughtFake++;
+            }
+        }
+
+        int errors = amountThoughtReal + amountThoughtFake;
+        // Checks first that all were checked within the time limit
+        if (manager.currentlyChecking < manager.current.numberOfItems)
+        {
+            nextLevelButton.interactable = false;
+            onTimeRanOut.Invoke();
+        }
+        else if (errors < manager.current.numberOfErrorsForFailure) // Checks how many were completed
+        {
+            if (errors <= 0)
+            {
+                onPerfectWin.Invoke();
+            }
+            else
+            {
+                onWin.Invoke();
+            }
+        }
+        else // If neither previous statement was true, player failed due to getting too many wrong
+        {
+            nextLevelButton.interactable = false;
+            onTooFewCheckedAccurately.Invoke();
+        }
+
+        requiredRightForSuccess.text = (manager.current.numberOfItems - manager.current.numberOfErrorsForFailure).ToString();
+        successful.text = finalScore.ToString();
+        thoughtWasReal.text = amountThoughtReal.ToString();
+        thoughtWasFake.text = amountThoughtFake.ToString();
+        remainingTime.text = manager.levelTimer.remaining.ToString();
+    }
+}
