f080bfe1795ffd6cdcd75a90677b21efe9f8f8ba
Marcus Mouyat <marcus@mouyat.com>
2022-02-27 00:52:20 +1100

    MenuHandler class for simple menu functions
    Functions include:
    * Switching windows easily with just one command, by referencing a list of all other canvas objects and enabling/disabling them accordingly
    * Toggling fullscreen mode
    * Quitting the game in a way that can be assigned as an event listener in the Inspector

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public Canvas rootWindow;
+    
+    List<Canvas> childWindows;
+    Canvas ownCanvas;
+
+    private void Awake()
+    {
+        ownCanvas = GetComponent<Canvas>();
+        childWindows = new List<Canvas>(GetComponentsInChildren<Canvas>(true));
+        childWindows.Remove(ownCanvas);
+
+        if (rootWindow == null || childWindows.Contains(rootWindow) == false)
+        {
+            rootWindow = childWindows[0];
+        }
+    }
+    private void Start()
+    {
+        SwitchWindow(rootWindow);
+    }
+
+    public void SwitchWindow(Canvas newWindow)
+    {
+        if (childWindows.Contains(newWindow) == false)
+        {
+            return;
+        }
+        for (int i = 0; i < childWindows.Count; i++)
+        {
+            childWindows[i].gameObject.SetActive(false);
+        }
+        newWindow.gameObject.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    
+
+    
+}
