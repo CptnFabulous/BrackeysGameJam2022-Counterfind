a08fd3748935fd72969c9e337e668b363c22fbad
Marcus Mouyat <marcus@mouyat.com>
2022-02-27 00:55:09 +1100

    ResolutionOptions class for setting up a dropdown menu to let the player change resolutions
    By assigning this MonoBehaviour class to a Dropdown object, the game automatically generates a formatted list of resolutions with no duplicates, generates the dropdown list to display them and adds a listener so it updates the resolution accordingly.

---
+++ Assets/Scripts/ResolutionOptions.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionOptions : MonoBehaviour
+{
+    public Dropdown GUIDropdown;
+
+    private void Awake()
+    {
+        SetupResolutionTable();
+        SetupResolutionDropdown(GUIDropdown);
+    }
+
+    public static List<Resolution> availableResolutions { get; private set; }
+    public static int maxRefreshRate { get; private set; }
+    public static void SetupResolutionTable()
+    {
+        List<Resolution> allResolutions = new List<Resolution>(Screen.resolutions);
+        allResolutions.Sort((lhs, rhs) => lhs.refreshRate.CompareTo(rhs.refreshRate));
+        maxRefreshRate = allResolutions[allResolutions.Count - 1].refreshRate;
+        availableResolutions = new List<Resolution>();
+        while (allResolutions.Count > 0)
+        {
+            Resolution res = allResolutions[0];
+            availableResolutions.Add(res);
+            allResolutions.RemoveAll((r) => r.width == res.width && r.height == res.height);
+        }
+        availableResolutions.Sort((a, b) => (a.width * a.height).CompareTo(b.width * b.height));
+    }
+    public static void ChangeResolution(int index)
+    {
+        Resolution toUse = availableResolutions[index];
+        Screen.SetResolution(toUse.width, toUse.height, Screen.fullScreen, maxRefreshRate);
+    }
+    public static void SetupResolutionDropdown(Dropdown dropdown)
+    {
+        List<string> resolutionNames = new List<string>();
+        for (int i = 0; i < availableResolutions.Count; i++)
+        {
+            resolutionNames.Add(availableResolutions[i].width + " X " + availableResolutions[i].height);
+        }
+
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutionNames);
+        dropdown.onValueChanged.AddListener(ChangeResolution);
+    }
+}
