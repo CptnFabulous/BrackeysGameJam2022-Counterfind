328c87eb073c3e24af6bd8fe03b243ba219f23f1
Marcus Mouyat <marcus@mouyat.com>
2022-06-15 15:08:37 +1000

    VolumeSlider script
    This component attaches to a Slider UI element, and acts as an easy way to alter game volume, with appropriate logarithmic adjustment! Just specify the audio mixer and the name of the exposed value you want to alter, and all other setup will be performed for you!

---
+++ Assets/Scripts/Menus/VolumeSlider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(Slider))]
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer mixerToAlter;
+    public string parameterName;
+    Slider slider;
+
+    private void Awake()
+    {
+        // References slider and adjusts variables to set up operation
+        slider = GetComponent<Slider>();
+        slider.minValue = float.Epsilon;
+        slider.maxValue = 1;
+        slider.onValueChanged.AddListener(SetValue);
+    }
+    /// <summary>
+    /// Logarithmically adjusts volume value using an input between 0 and 1.
+    /// </summary>
+    /// <param name="value"></param>
+    public void SetValue(float value)
+    {
+        value = Mathf.Clamp(value, float.Epsilon, 1); // Clamps between one and lowest non-zero value (logarithmic code is apparently weird with zero values)
+        value = Mathf.Log10(value) * 20; // Alters value to account for logarithmic sound scale
+        mixerToAlter.SetFloat(parameterName, value);
+    }
+}
