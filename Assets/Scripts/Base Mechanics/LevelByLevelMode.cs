08105fa5e1273b2c48ed542e6b281993466a814f
Marcus Mouyat <marcus@mouyat.com>
2022-06-16 14:48:59 +1000

    LevelByLevelMode class to decouple level-based gameplay mechanics from general gameplay controls
    This script inherits from Gamemode, and includes all the functionality for loading a gameplay scenario based on a level. This functionality is already present in the class currently named LevelManager, but it will be removed from that class to decouple it from the code for gameplay controls, and allow other non-level-based gameplay modes to be implemented. I plan on adding an infinite mode, and may also implement a zen mode with no failure state.

---
+++ Assets/Scripts/Base Mechanics/LevelByLevelMode.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelByLevelMode : Gamemode
+{
+    public Level currentLevel { get; private set; }
+    public bool[] judgedFakeByPlayer { get; private set; }
+    public int currentlyChecking { get; private set; }
+
+    public bool allNotesChecked => currentlyChecking >= allItems.Length - 1;
+
+    public void SetLevel(Level newLevel)
+    {
+        currentLevel = newLevel;
+        gameElements.currentMode = this;
+    }
+
+    public override List<Banknote.Defect> Defects() => Banknote.FromFlags(currentLevel.defects);
+    public override Banknote CurrentItem()
+    {
+        bool indexIsInArray = currentlyChecking >= 0;
+        return notesExist && indexIsInArray ? allItems[currentlyChecking] : null;
+    }
+    public override Banknote NextItem()
+    {
+        // Determine if notes are available, and if some have not yet been checked
+        return (notesExist && allNotesChecked == false) ? allItems[currentlyChecking + 1] : null;
+    }
+
+    public override void GenerateGamemodeElements()
+    {
+        // Purge existing notes (code could probably be done better to prevent garbage collection but whatever it's a game jam)
+        if (allItems != null)
+        {
+            for (int i = 0; i < allItems.Length; i++)
+            {
+                Destroy(allItems[i].gameObject);
+            }
+        }
+
+        List<Banknote> newNotes = new List<Banknote>();
+        for (int i = 0; i < currentLevel.numberOfItems; i++)
+        {
+            // Spawn note object
+            Banknote note = Instantiate(gameElements.prefab, gameElements.entryPilePosition);
+            note.transform.localPosition = Vector3.zero;
+            note.transform.localRotation = Quaternion.identity;
+
+            // If index is less than number of counterfeits, mark as counterfeit
+            // To ensure the correct amount
+            note.GenerateNote((i < currentLevel.numberOfCounterfeits), currentLevel);
+
+            // Insert at a random point to shuffle the array
+            newNotes.Insert(Random.Range(0, newNotes.Count), note);
+        }
+        allItems = newNotes.ToArray();
+        judgedFakeByPlayer = new bool[allItems.Length];
+        currentlyChecking = -1;
+
+        // Reset timer
+        gameElements.levelTimer.timeLimit = currentLevel.timeLimit;
+    }
+    public override void OnJudgementMade(bool deemedCounterfeit)
+    {
+        judgedFakeByPlayer[currentlyChecking] = deemedCounterfeit;
+
+        if (allNotesChecked)
+        {
+            gameElements.levelTimer.Pause();
+        }
+    }
+    public override void PrepareNextItem()
+    {
+        if (NextItem() != null) // If a new item is present, increase currentlyChecking so that one is registered
+        {
+            currentlyChecking++;
+        }
+        else
+        {
+            EndGameplay();
+        }
+    }
+    public override void EndGameplay()
+    {
+        gameElements.EndLevel();
+    }
+
+
+
+    
+
+
+    private void LateUpdate()
+    {
+        if (currentLevel == null)
+        {
+            return;
+        }
+        gameElements.remainingNotes.text = (allItems.Length - currentlyChecking - 1).ToString();
+    }
+
+    
+
+}
