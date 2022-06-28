b52e74cad48f09e2843e1baf2f75095aabbb8ec5
Marcus Mouyat <marcus@mouyat.com>
2022-06-28 18:53:46 +1000

    ToggleTransition class for smoothly switching between two different states based on a bool
    This script makes a float value shift between one and zero, based on if the 'active' bool is enabled or disabled. This acts as an easy way for toggleable mechanics to still have a transition state, e.g. for animations.

---
+++ Assets/Scripts/Miscellaneous/ToggleTransition.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTransition : MonoBehaviour
+{
+    public float transitionTime = 0.25f;
+    public AnimationCurve transitionCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    public UnityEngine.Events.UnityEvent<float> effect;
+
+    bool currentlyActive;
+    float currentValue;
+    IEnumerator currentTransition;
+
+    float stateTarget => active ? 1 : 0; // What the timer value is transitioning towards
+    bool mustTransition => currentValue != stateTarget;
+    public bool active
+    {
+        get => currentlyActive;
+        set
+        {
+            // Start the transition coroutine, but only if an existing transition coroutine is not active
+            // The transition coroutine will automatically shift to the desired value if it changes
+            currentlyActive = value;
+            if (mustTransition && currentTransition == null)
+            {
+                currentTransition = Transition();
+                StartCoroutine(currentTransition);
+            }
+        }
+    }
+    
+
+    IEnumerator Transition()
+    {
+        while (mustTransition)
+        {
+            currentValue = Mathf.MoveTowards(currentValue, stateTarget, Time.deltaTime / transitionTime);
+            float t = transitionCurve.Evaluate(currentValue);
+            effect.Invoke(t); // Invoke function
+
+            yield return null;
+        }
+
+        currentTransition = null; // Nulls value at the end so the 'active' setter can tell there's no coroutine active
+    }
+}
