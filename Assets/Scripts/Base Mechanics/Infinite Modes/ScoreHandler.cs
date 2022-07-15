34034e03590dc6ebfa2c5ce68d425b47d094e057
Marcus Mouyat <marcus@mouyat.com>
2022-07-15 15:34:53 +1000

    Tweaked current score value in ScoreHandler to be a publicly gettable property rather than a field
    This is so it can be easily referenced in the game over screen.

--- Assets/Scripts/Base Mechanics/Infinite Modes/ScoreHandler.cs
+++ Assets/Scripts/Base Mechanics/Infinite Modes/ScoreHandler.cs
@@ -8,24 +8,25 @@ public class ScoreHandler : JudgementHandler
     [Header("Score")]
     public int scorePerNote = 100; // Score awarded per note
     public float consecutiveMultiplierIncrease = 0.1f; // Score multiplier increase for each consecutive correct note
-    int score; // The current score
+    
 
     [Header("Cosmetics")]
     public Text scoreCounter;
     public Text streakCounter;
 
+    public int currentScore { get; private set; } // The current score
     public int successStreak { get; private set; } // How many notes has the player gotten correct in a row?
     public float currentMultiplier => 1 + (consecutiveMultiplierIncrease * successStreak);
     public override void OnResetGame()
     {
-        score = 0;
+        currentScore = 0;
         successStreak = 0;
         base.OnResetGame();
     }
 
     public override void OnCorrect()
     {
-        score += Mathf.CeilToInt(scorePerNote * currentMultiplier);
+        currentScore += Mathf.CeilToInt(scorePerNote * currentMultiplier);
         successStreak++;
         base.OnCorrect();
     }
@@ -38,8 +39,8 @@ public class ScoreHandler : JudgementHandler
 
     public override void UpdateHUD()
     {
-        scoreCounter.text = score.ToString();
-        Debug.Log("Current score = " + score + ", multiplier = " + currentMultiplier);
+        scoreCounter.text = currentScore.ToString();
         streakCounter.text = "X" + successStreak;
+        Debug.Log("Current score = " + currentScore + ", multiplier = " + currentMultiplier);
     }
 }
