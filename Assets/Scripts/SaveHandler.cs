3bf27b63f12213cabb04aa9e37257b8245d1f7c1
Marcus Mouyat <marcus@mouyat.com>
2022-07-22 17:26:15 +1000

    (UNTESTED) SaveHandler class for saving and loading data
    This static class will save game data to and load it from a JSON file. Saving and loading are performed effortlessly using Save() and Load() functions, which will assign the data to an appropriate file based on an index.
    Save data is stored during runtime in custom 'SaveFile' structs. A public static instance of this struct, named 'currentData', can be read from and written to by any other game element, when important data needs to be updated. Two static System.Actions, onLoadGame and onSaveGame, allow any number of actions to occur on saving or loading, by adding them as listeners.
    ‘currentData’, ‘onLoadGame’ and ‘onSaveGame’ allow the rest of the code to effortlessly read from or write to save data, and act on the player saving or loading, without the system itself needing to reference any other class or scene object.
    
    (The SaveFile class presently has no variables included in this commit, because they aren't relevant to the way the save system works)

---
+++ Assets/Scripts/SaveHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class SaveHandler
+{
+    public struct SaveFile
+    {
+
+    }
+
+    public static SaveFile currentData;
+    static int saveIndex = 0;
+
+    /// <summary>
+    /// Invoked whenever a save file is loaded.
+    /// </summary>
+    public static System.Action<SaveFile> onLoadGame;
+    /// <summary>
+    /// Invoked whenever the game is saved.
+    /// </summary>
+    public static System.Action<SaveFile> onSaveGame;
+
+    /// <summary>
+    /// Saves data held in currentData to a JSON file.
+    /// </summary>
+    public static void Save()
+    {
+        string jsonString = JsonUtility.ToJson(currentData);
+        StreamWriter streamWriter = new StreamWriter(FilePath(saveIndex));
+        streamWriter.Write(jsonString);
+        onSaveGame.Invoke(currentData);
+    }
+    /// <summary>
+    /// Loads data from the correct JSON file into currentData.
+    /// </summary>
+    public static void Load()
+    {
+        StreamReader streamReader = new StreamReader(FilePath(saveIndex));
+        string jsonString = streamReader.ReadToEnd();
+        currentData = JsonUtility.FromJson<SaveFile>(jsonString);
+        onLoadGame.Invoke(currentData);
+    }
+
+    static string FilePath(int index) => Application.persistentDataPath + "/" + fileName + index + extension;
+    const string fileName = "SaveData";
+    const string extension = ".json";
+}
