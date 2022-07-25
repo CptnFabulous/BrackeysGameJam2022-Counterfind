fddc0706ec408bbeeaf0297577f26e09a0ffdf72
Marcus Mouyat <marcus@mouyat.com>
2022-07-25 18:55:43 +1000

    Tweaked EndScreen class to work with ObjectToggleGroup class
    This is since only one end screen object should ever be active at a time, depending on the current gamemode.

--- Assets/Scripts/Base Mechanics/EndScreen.cs
+++ Assets/Scripts/Base Mechanics/EndScreen.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public abstract class EndScreen : MonoBehaviour
 {
+    public ObjectToggleGroup toggleGroup;
+    [Header("General")]
     public Text reason;
     public Button retryButton;
     public Button quitButton;
     
     public virtual void Awake()
     {
+        toggleGroup?.allObjects.Add(gameObject);
         retryButton.onClick.AddListener(LevelProgressionHandler.Current.RetryLevel);
         quitButton.onClick.AddListener(LevelProgressionHandler.Current.ReturnToMenu);
     }
     public virtual void Generate()
     {
+        toggleGroup?.SetActiveObject(gameObject);
         gameObject.SetActive(true);
         reason.text = "";
     }
