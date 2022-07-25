d4526871f69b49516b78d20a711d70d29cb2a6dd
Marcus Mouyat <marcus@mouyat.com>
2022-07-25 14:13:41 +1000

    'Player' script to simplify player references
    This MonoBehaviour consolidates references to player data such as controls, stats and HUD elements. This is used so other game elements can easily reference the player without needing to establish separate references to each component. This is important for decoupling the player and game elements.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Camera viewCamera;
+    public GameStateHandler stateHandler;
+
+    [Header("Game controls")]
+    public ObjectViewer objectViewer;
+    public Button judgeReal;
+    public Button judgeFake;
+    public ReferenceWindow referenceWindow;
+
+    [Header("Game HUD")]
+    public Text timerDisplay;
+    public Text noteCounter;
+}
